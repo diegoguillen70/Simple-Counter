acb77b4af51a32be727e1f2efac32bb112d5832e
diegoguillen70 <diegoguillen70@gmail.com>
2023-08-22 09:52:41 -0400

    project delivered

--- src/js/component/SecondsCounter/index.jsx
+++ src/js/component/SecondsCounter/index.jsx
@@ -12,43 +12,56 @@ const SecondsCounter = () => {
     for (const key in element) {
       arrayDigits.push(element[key]);
     }
-
     //console.log(arrayDigits);
   });
   return (
-    <div className="container">
-      <h1 className="set-timer">
-        <i className="fa-solid fa-clock"></i>
-      </h1>
-      <h1 className="stop-resume">
-        <i className="fa-solid fa-stopwatch" onClick={() => stopResume()}></i>
-      </h1>
-      <h1 className="reset">
-        <i className="fa-solid fa-xmark " onClick={() => resect()}></i>
-      </h1>
+    <>
+      <div className="container">
+        <h1 className="set-timer">
+          <i className="fa-solid fa-clock"></i>
+        </h1>
+        <h1 className="stop-resume">
+          <i className="fa-solid fa-stopwatch" onClick={() => stopResume()}></i>
+        </h1>
+        <h1 className="reset">
+          <i className="fa-solid fa-xmark " onClick={() => resect()}></i>
+        </h1>
 
-      {arrayDigits.map((digits, index) => {
-        //console.log(index);
-        if (index == 0 || index == 2) {
-          return (
-            <>
-              <h1 key={index} className="digit">
-                {digits}
-              </h1>
-              <h1 className="colon">:</h1>
-            </>
-          );
-        } else {
-          return (
-            <>
-              <h1 key={index} className="digit">
-                {digits}
-              </h1>
-            </>
-          );
-        }
-      })}
-    </div>
+        {arrayDigits.map((digits, index) => {
+          //console.log(index);
+          if (index == 0 || index == 2) {
+            return (
+              <>
+                <h1 key={index} className="digit">
+                  {digits}
+                </h1>
+                <h1 className="colon">:</h1>
+              </>
+            );
+          } else {
+            return (
+              <>
+                <h1 key={index} className="digit">
+                  {digits}
+                </h1>
+              </>
+            );
+          }
+        })}
+      </div>
+      {/*
+      <div className="set-timer-box">
+        <i class="fa-solid fa-circle-xmark"></i>
+        <label htmlFor=""></label>
+        <input type="text" />
+        <label htmlFor=""></label>
+        <input type="text" />
+        <span className="button">
+          <button>Set Time</button>
+          <button>Cancel</button>
+        </span>
+      </div>*/}
+    </>
   );
 };
 
