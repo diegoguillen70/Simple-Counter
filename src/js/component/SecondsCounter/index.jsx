26377c96d28c252bed08cc1cbc520bd22a644383
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-08-22 14:56:31 +0000

    "fixed height"

--- src/js/component/SecondsCounter/index.jsx
+++ src/js/component/SecondsCounter/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./timerStyle.css";
 import useTimer from "../../hooks/useTimer";
-import { digitConvert } from "/home/diego/Documents/Git Repository/React/Simple-Counter/src/js/functions/functions.js";
+import { digitConvert } from "/workspaces/Simple-Counter/src/js/functions/functions.js";
 const SecondsCounter = () => {
   const { seconds, stopResume, resect } = useTimer();
   //const objectDigits = digitConvert(seconds);
